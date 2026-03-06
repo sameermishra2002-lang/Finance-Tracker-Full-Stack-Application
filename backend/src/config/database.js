3dec7a39ca3407c5b2548146cece1fef1ea30464
sameermishra2002-lang <sameermishra2002@gmail.com>
2026-03-07 02:13:03 +0530

    hosting redis

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -12,12 +12,15 @@ const { Pool } = pg;
 
 // Create connection pool
 export const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5433,
+  host: process.env.DB_HOST,          //    ||'localhost',
+  port: process.env.DB_PORT,          //    || 5433,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  max: 20, // Maximum number of clients in pool
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
+  max: 20,                                // Maximum number of clients in pool
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
